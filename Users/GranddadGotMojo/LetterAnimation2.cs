d7baeeb00082114c4679c827c994062976b5e09a
Mike Suttom <mike.j.sutton@tx.rr.com>
2019-05-22 09:17:19 -0500

    Add files via upload

---
+++ Users/GranddadGotMojo/LetterAnimation2.cs
@@ -0,0 +1,328 @@
+/* This content is licensed under the terms of the Creative Commons Attribution 4.0 International License.
+ * When using this content, you must:
+ * •    Acknowledge that the content is from the Sansar Knowledge Base.
+ * •    Include our copyright notice: "© 2017 Linden Research, Inc."
+ * •    Indicate that the content is licensed under the Creative Commons Attribution-Share Alike 4.0 International License.
+ * •    Include the URL for, or link to, the license summary at https://creativecommons.org/licenses/by-sa/4.0/deed.hi (and, if possible, to the complete license terms at https://creativecommons.org/licenses/by-sa/4.0/legalcode.
+ * For example:
+ * "This work uses content from the Sansar Knowledge Base. © 2017 Linden Research, Inc. Licensed under the Creative Commons Attribution 4.0 International License (license summary available at https://creativecommons.org/licenses/by/4.0/ and complete license terms available at https://creativecommons.org/licenses/by/4.0/legalcode)."
+ */
+
+using Sansar.Script;
+using Sansar.Simulation;
+using Sansar;
+using System;
+using System.Linq;
+using System.Collections.Generic;
+
+public class LetterAnimation2 : SceneObjectScript
+{
+    #region EditorProperties
+    // Start playing on these events. Can be a comma separated list of event names.
+    [DisplayName("Letter Position")]
+    [DefaultValue(1)]
+    public int LetterPos;
+
+    public string EnableEvent = null;
+    public string DisableEvent = null;
+
+    string LetterIn = null;
+    private readonly string[] Letters = { "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", " "};
+
+
+    public interface iSendChar
+    {
+        int CharIndex { get; }
+        string CharToSend { get; }
+    }
+
+    #endregion
+
+    #region SimpleHelpers v2
+    // Update the region tag above by incrementing the version when updating anything in the region.
+
+    // If a Group is set, will only respond and send to other SimpleScripts with the same Group tag set.
+    // Does NOT accept CSV lists of groups.
+    // To send or receive events to/from a specific group from outside that group prepend the group name with a > to the event name
+    // my_group>on
+    [DefaultValue("")]
+    [DisplayName("Group")]
+    public string Group = "";
+
+    public interface ISimpleData
+    {
+        AgentInfo AgentInfo { get; }
+        ObjectId ObjectId { get; }
+        ObjectId SourceObjectId { get; }
+
+        // Extra data
+        Reflective ExtraData { get; }
+    }
+
+    public class SimpleData : Reflective, ISimpleData
+    {
+        public SimpleData(ScriptBase script) { ExtraData = script; }
+        public AgentInfo AgentInfo { get; set; }
+        public ObjectId ObjectId { get; set; }
+        public ObjectId SourceObjectId { get; set; }
+
+        public Reflective ExtraData { get; }
+    }
+
+    public interface IDebugger { bool DebugSimple { get; } }
+    private bool __debugInitialized = false;
+    private bool __SimpleDebugging = false;
+    private string __SimpleTag = "";
+
+    private string GenerateEventName(string eventName)
+    {
+        eventName = eventName.Trim();
+        if (eventName.EndsWith("@"))
+        {
+            // Special case on@ to send the event globally (the null group) by sending w/o the @.
+            return eventName.Substring(0, eventName.Length - 1);
+        }
+        else if (Group == "" || eventName.Contains("@"))
+        {
+            // No group was set or already targeting a specific group as is.
+            return eventName;
+        }
+        else
+        {
+            // Append the group
+            return $"{eventName}@{Group}";
+        }
+    }
+
+    private void SetupSimple()
+    {
+        __debugInitialized = true;
+        __SimpleTag = GetType().Name + " [S:" + Script.ID.ToString() + " O:" + ObjectPrivate.ObjectId.ToString() + "]";
+        Wait(TimeSpan.FromSeconds(1));
+        IDebugger debugger = ScenePrivate.FindReflective<IDebugger>("SimpleDebugger").FirstOrDefault();
+        if (debugger != null) __SimpleDebugging = debugger.DebugSimple;
+    }
+
+    System.Collections.Generic.Dictionary<string, Func<string, Action<ScriptEventData>, Action>> __subscribeActions = new System.Collections.Generic.Dictionary<string, Func<string, Action<ScriptEventData>, Action>>();
+    private Action SubscribeToAll(string csv, Action<ScriptEventData> callback)
+    {
+        if (!__debugInitialized) SetupSimple();
+        if (string.IsNullOrWhiteSpace(csv)) return null;
+
+        Func<string, Action<ScriptEventData>, Action> subscribeAction;
+        if (__subscribeActions.TryGetValue(csv, out subscribeAction))
+        {
+            return subscribeAction(csv, callback);
+        }
+
+        // Simple case.
+        if (!csv.Contains(">>"))
+        {
+            __subscribeActions[csv] = SubscribeToAllInternal;
+            return SubscribeToAllInternal(csv, callback);
+        }
+
+        // Chaining
+        __subscribeActions[csv] = (_csv, _callback) =>
+        {
+            System.Collections.Generic.List<string> chainedCommands = new System.Collections.Generic.List<string>(csv.Split(new string[] { ">>" }, StringSplitOptions.RemoveEmptyEntries));
+
+            string initial = chainedCommands[0];
+            chainedCommands.RemoveAt(0);
+            chainedCommands.Add(initial);
+
+            Action unsub = null;
+            Action<ScriptEventData> wrappedCallback = null;
+            wrappedCallback = (data) =>
+            {
+                string first = chainedCommands[0];
+                chainedCommands.RemoveAt(0);
+                chainedCommands.Add(first);
+                if (unsub != null) unsub();
+                unsub = SubscribeToAllInternal(first, wrappedCallback);
+                Log.Write(LogLevel.Info, "CHAIN Subscribing to " + first);
+                _callback(data);
+            };
+
+            unsub = SubscribeToAllInternal(initial, wrappedCallback);
+            return unsub;
+        };
+
+        return __subscribeActions[csv](csv, callback);
+    }
+
+    private Action SubscribeToAllInternal(string csv, Action<ScriptEventData> callback)
+    {
+        Action unsubscribes = null;
+        string[] events = csv.Trim().Split(new char[] { ',' }, StringSplitOptions.RemoveEmptyEntries);
+        if (__SimpleDebugging)
+        {
+            Log.Write(LogLevel.Info, __SimpleTag, "Subscribing to " + events.Length + " events: " + string.Join(", ", events));
+        }
+        Action<ScriptEventData> wrappedCallback = callback;
+
+        foreach (string eventName in events)
+        {
+            if (__SimpleDebugging)
+            {
+                var sub = SubscribeToScriptEvent(GenerateEventName(eventName), (ScriptEventData data) =>
+                {
+                    Log.Write(LogLevel.Info, __SimpleTag, "Received event " + GenerateEventName(eventName));
+                    wrappedCallback(data);
+                });
+                unsubscribes += sub.Unsubscribe;
+            }
+            else
+            {
+                var sub = SubscribeToScriptEvent(GenerateEventName(eventName), wrappedCallback);
+                unsubscribes += sub.Unsubscribe;
+            }
+        }
+        return unsubscribes;
+    }
+
+    System.Collections.Generic.Dictionary<string, Action<string, Reflective>> __sendActions = new System.Collections.Generic.Dictionary<string, Action<string, Reflective>>();
+    private void SendToAll(string csv, Reflective data)
+    {
+        if (!__debugInitialized) SetupSimple();
+        if (string.IsNullOrWhiteSpace(csv)) return;
+
+        Action<string, Reflective> sendAction;
+        if (__sendActions.TryGetValue(csv, out sendAction))
+        {
+            sendAction(csv, data);
+            return;
+        }
+
+        // Simple case.
+        if (!csv.Contains(">>"))
+        {
+            __sendActions[csv] = SendToAllInternal;
+            SendToAllInternal(csv, data);
+            return;
+        }
+
+        // Chaining
+        System.Collections.Generic.List<string> chainedCommands = new System.Collections.Generic.List<string>(csv.Split(new string[] { ">>" }, StringSplitOptions.RemoveEmptyEntries));
+        __sendActions[csv] = (_csv, _data) =>
+        {
+            string first = chainedCommands[0];
+            chainedCommands.RemoveAt(0);
+            chainedCommands.Add(first);
+
+            Log.Write(LogLevel.Info, "CHAIN Sending to " + first);
+            SendToAllInternal(first, _data);
+        };
+        __sendActions[csv](csv, data);
+    }
+
+    private void SendToAllInternal(string csv, Reflective data)
+    {
+        if (string.IsNullOrWhiteSpace(csv)) return;
+        string[] events = csv.Trim().Split(new char[] { ',' }, StringSplitOptions.RemoveEmptyEntries);
+
+        if (__SimpleDebugging) Log.Write(LogLevel.Info, __SimpleTag, "Sending " + events.Length + " events: " + string.Join(", ", events) + (Group != "" ? (" to group " + Group) : ""));
+        foreach (string eventName in events)
+        {
+            PostScriptEvent(GenerateEventName(eventName), data);
+        }
+    }
+    #endregion
+
+    private Animation animation = null;
+    private AnimationParameters initialAnimationParameters;
+
+    private AnimationComponent animComponent;
+
+    public override void Init()
+    {
+        int stuff = 0;
+        if (!ObjectPrivate.TryGetFirstComponent<AnimationComponent>(out animComponent))
+        {
+            Log.Write(LogLevel.Error, "NPCAnimation.Init", "Object must have an animation added at edit time for NPCAnimation to work");
+            return;
+        }
+
+        animation = animComponent.DefaultAnimation;
+
+        initialAnimationParameters = animation.GetParameters();
+        //SubscribeToAll("Kill", KillAnimation);
+        animation.JumpToFrame(36);
+
+        if (EnableEvent != "")
+        {
+            Log.Write("Enable Event was not null: " + EnableEvent);
+            SubscribeToScriptEvent("LetterSent", getLetter);
+        }
+        else
+        {
+            Log.Write("Enable Event was null: " + EnableEvent);
+            SubscribeToScriptEvent("LetterSent", getLetter);
+            //Subscribe(null);  //executes it by passing null data
+
+        }
+
+        if (DisableEvent != "")
+        {
+            //SubscribeToScriptEvent("LetterSent", getLetter);
+            //SubscribeToAll(DisableEvent, Unsubscribe);
+
+        }
+    }
+
+    private void getLetter(ScriptEventData gotLetter)
+    {
+
+        //Log.Write("ActionSynthModule: In getKeyInfo");
+        if (gotLetter.Data == null)
+        {
+            return;
+        }
+
+        iSendChar sentLetter = gotLetter.Data.AsInterface<iSendChar>();
+
+        if (sentLetter == null)
+        {
+            Log.Write(LogLevel.Error, Script.ID.ToString(), "Unable to create interface, check logs for missing member(s)");
+            return;
+        }
+
+        Log.Write("CharIndex: " + sentLetter.CharIndex + " LetterPos: " + LetterPos);
+        if (sentLetter.CharIndex == LetterPos)
+        {
+            LetterIn = sentLetter.CharToSend;
+            LetterIn = LetterIn.ToUpper();
+            int cntr = 0;
+            Log.Write("LetterIn: " + LetterIn);
+            do
+            {
+                if (LetterIn == Letters[cntr])
+                {
+                    Log.Write("LetterIn: " + LetterIn + " Letters[" + cntr + "]: " + Letters[cntr]);
+                    animation.JumpToFrame(cntr);
+                    break;
+                }
+                cntr++;
+            } while (cntr < Letters.Count());
+        }
+  
+        //Log.Write("KeyIn before Trim: " + KeyIn);
+
+    }
+
+    private void Subscribe(ScriptEventData sed)  //doesn't really pass data.  Always passes null
+    {
+        //Look At Animation Strings and subscribe to events
+        Log.Write("In Subscribe");
+
+        animation.JumpToFrame(36);
+    }
+
+    private void Unsubscribe(ScriptEventData sed)
+    {
+
+    }
+
+}
+
+
---
+++ Users/GranddadGotMojo/LettersDisplayPhrase.cs
@@ -0,0 +1,301 @@
+﻿//* "This work uses content from the Sansar Knowledge Base. © 2017 Linden Research, Inc. Licensed under the Creative Commons Attribution 4.0 International License (license summary available at https://creativecommons.org/licenses/by/4.0/ and complete license terms available at https://creativecommons.org/licenses/by/4.0/legalcode)."
+
+#define SansarBuild
+
+using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Diagnostics;
+
+using Sansar;
+using Sansar.Script;
+using Sansar.Simulation;
+
+public class LetterDisplayPhrase : SceneObjectScript
+{
+    #region ConstantsVariables
+
+    [DefaultValue("ALL")]
+    [DisplayName("Valid User List:")]
+    public string UsersToListenTo = "ALL";
+
+
+    private List<string> ValidUsers = new List<string>();
+
+    private string Errormsg = "No Errors";
+    private bool strErrors = false;
+    private SessionId Jammer = new SessionId();
+    //private AgentInfo AgentName;
+    private Action SimpleScriptSubscription;
+    AgentInfo AgentName;
+    ObjectId ComponentID;
+
+    public class SendChar : Reflective
+    {
+        public int CharIndex { get; set; }
+        public string CharToSend { get; set; }
+    }
+
+
+    #endregion
+
+    public override void Init()
+    {
+
+        //myVolume = ScenePrivate.PlayStream(StreamChannel.MediaChannel, 0);
+
+        Script.UnhandledException += UnhandledException; // Catch errors and keep running unless fatal
+        //ScenePrivate.Chat.MessageAllUsers(WelcomeMessage);
+        ScenePrivate.Chat.Subscribe(0, GetChatCommand);
+    }
+
+
+    private void UnhandledException(object Sender, Exception Ex)
+    {
+
+        Log.Write(LogLevel.Error, GetType().Name, Ex.Message + "\n" + Ex.StackTrace + "\n" + Ex.Source);
+        return;
+    }//UnhandledException
+
+    #region Communication
+
+    #region SimpleHelpers v2
+    // Update the region tag above by incrementing the version when updating anything in the region.
+
+    // If a Group is set, will only respond and send to other SimpleScripts with the same Group tag set.
+    // Does NOT accept CSV lists of groups.
+    // To send or receive events to/from a specific group from outside that group prepend the group name with a > to the event name
+    // my_group>on
+    [DefaultValue("")]
+    [DisplayName("Group")]
+    public string Group = "";
+
+    public interface ISimpleData
+    {
+        AgentInfo AgentInfo { get; }
+        ObjectId ObjectId { get; }
+        ObjectId SourceObjectId { get; }
+
+        // Extra data
+        Reflective ExtraData { get; }
+    }
+
+    public class SimpleData : Reflective, ISimpleData
+    {
+        public SimpleData(ScriptBase script) { ExtraData = script; }
+        public AgentInfo AgentInfo { get; set; }
+        public ObjectId ObjectId { get; set; }
+        public ObjectId SourceObjectId { get; set; }
+
+        public Reflective ExtraData { get; }
+    }
+
+    public interface IDebugger { bool DebugSimple { get; } }
+    private bool __debugInitialized = false;
+    private bool __SimpleDebugging = false;
+    private string __SimpleTag = "";
+
+    private string GenerateEventName(string eventName)
+    {
+        eventName = eventName.Trim();
+        if (eventName.EndsWith("@"))
+        {
+            // Special case on@ to send the event globally (the null group) by sending w/o the @.
+            return eventName.Substring(0, eventName.Length - 1);
+        }
+        else if (Group == "" || eventName.Contains("@"))
+        {
+            // No group was set or already targeting a specific group as is.
+            return eventName;
+        }
+        else
+        {
+            // Append the group
+            return $"{eventName}@{Group}";
+        }
+    }
+
+    private void SetupSimple()
+    {
+        __debugInitialized = true;
+        __SimpleTag = GetType().Name + " [S:" + Script.ID.ToString() + " O:" + ObjectPrivate.ObjectId.ToString() + "]";
+        Wait(TimeSpan.FromSeconds(1));
+        IDebugger debugger = ScenePrivate.FindReflective<IDebugger>("SimpleDebugger").FirstOrDefault();
+        if (debugger != null) __SimpleDebugging = debugger.DebugSimple;
+    }
+
+    System.Collections.Generic.Dictionary<string, Func<string, Action<ScriptEventData>, Action>> __subscribeActions = new System.Collections.Generic.Dictionary<string, Func<string, Action<ScriptEventData>, Action>>();
+    private Action SubscribeToAll(string csv, Action<ScriptEventData> callback)
+    {
+        if (!__debugInitialized) SetupSimple();
+        if (string.IsNullOrWhiteSpace(csv)) return null;
+
+        Func<string, Action<ScriptEventData>, Action> subscribeAction;
+        if (__subscribeActions.TryGetValue(csv, out subscribeAction))
+        {
+            return subscribeAction(csv, callback);
+        }
+
+        // Simple case.
+        if (!csv.Contains(">>"))
+        {
+            __subscribeActions[csv] = SubscribeToAllInternal;
+            return SubscribeToAllInternal(csv, callback);
+        }
+
+        // Chaining
+        __subscribeActions[csv] = (_csv, _callback) =>
+        {
+            System.Collections.Generic.List<string> chainedCommands = new System.Collections.Generic.List<string>(csv.Split(new string[] { ">>" }, StringSplitOptions.RemoveEmptyEntries));
+
+            string initial = chainedCommands[0];
+            chainedCommands.RemoveAt(0);
+            chainedCommands.Add(initial);
+
+            Action unsub = null;
+            Action<ScriptEventData> wrappedCallback = null;
+            wrappedCallback = (data) =>
+            {
+                string first = chainedCommands[0];
+                chainedCommands.RemoveAt(0);
+                chainedCommands.Add(first);
+                if (unsub != null) unsub();
+                unsub = SubscribeToAllInternal(first, wrappedCallback);
+                Log.Write(LogLevel.Info, "CHAIN Subscribing to " + first);
+                _callback(data);
+            };
+
+            unsub = SubscribeToAllInternal(initial, wrappedCallback);
+            return unsub;
+        };
+
+        return __subscribeActions[csv](csv, callback);
+    }
+
+    private Action SubscribeToAllInternal(string csv, Action<ScriptEventData> callback)
+    {
+        Action unsubscribes = null;
+        string[] events = csv.Trim().Split(new char[] { ',' }, StringSplitOptions.RemoveEmptyEntries);
+        if (__SimpleDebugging)
+        {
+            Log.Write(LogLevel.Info, __SimpleTag, "Subscribing to " + events.Length + " events: " + string.Join(", ", events));
+        }
+        Action<ScriptEventData> wrappedCallback = callback;
+
+        foreach (string eventName in events)
+        {
+            if (__SimpleDebugging)
+            {
+                var sub = SubscribeToScriptEvent(GenerateEventName(eventName), (ScriptEventData data) =>
+                {
+                    Log.Write(LogLevel.Info, __SimpleTag, "Received event " + GenerateEventName(eventName));
+                    wrappedCallback(data);
+                });
+                unsubscribes += sub.Unsubscribe;
+            }
+            else
+            {
+                var sub = SubscribeToScriptEvent(GenerateEventName(eventName), wrappedCallback);
+                unsubscribes += sub.Unsubscribe;
+            }
+        }
+        return unsubscribes;
+    }
+
+    System.Collections.Generic.Dictionary<string, Action<string, Reflective>> __sendActions = new System.Collections.Generic.Dictionary<string, Action<string, Reflective>>();
+    private void SendToAll(string csv, Reflective data)
+    {
+        if (!__debugInitialized) SetupSimple();
+        if (string.IsNullOrWhiteSpace(csv)) return;
+
+        Action<string, Reflective> sendAction;
+        if (__sendActions.TryGetValue(csv, out sendAction))
+        {
+            sendAction(csv, data);
+            return;
+        }
+
+        // Simple case.
+        if (!csv.Contains(">>"))
+        {
+            __sendActions[csv] = SendToAllInternal;
+            SendToAllInternal(csv, data);
+            return;
+        }
+
+        // Chaining
+        System.Collections.Generic.List<string> chainedCommands = new System.Collections.Generic.List<string>(csv.Split(new string[] { ">>" }, StringSplitOptions.RemoveEmptyEntries));
+        __sendActions[csv] = (_csv, _data) =>
+        {
+            string first = chainedCommands[0];
+            chainedCommands.RemoveAt(0);
+            chainedCommands.Add(first);
+
+            Log.Write(LogLevel.Info, "CHAIN Sending to " + first);
+            SendToAllInternal(first, _data);
+        };
+        __sendActions[csv](csv, data);
+    }
+
+    private void SendToAllInternal(string csv, Reflective data)
+    {
+        if (string.IsNullOrWhiteSpace(csv)) return;
+        string[] events = csv.Trim().Split(new char[] { ',' }, StringSplitOptions.RemoveEmptyEntries);
+
+        if (__SimpleDebugging) Log.Write(LogLevel.Info, __SimpleTag, "Sending " + events.Length + " events: " + string.Join(", ", events) + (Group != "" ? (" to group " + Group) : ""));
+        foreach (string eventName in events)
+        {
+            PostScriptEvent(GenerateEventName(eventName), data);
+        }
+    }
+    #endregion
+
+    #endregion
+
+    private void GetChatCommand(ChatData Data)
+    {
+        Log.Write("Chat From: " + Data.SourceId);
+        Log.Write("Chat person: " + ScenePrivate.FindAgent(Data.SourceId).AgentInfo.Name);
+        AgentPrivate agent = ScenePrivate.FindAgent(Data.SourceId);
+        ValidUsers.Clear();
+        ValidUsers  = UsersToListenTo.Split(',').ToList();
+        if (UsersToListenTo.Contains("ALL"))
+        {
+            string DataCmd = Data.Message;
+            Log.Write("DataCmd: " + DataCmd);
+            ParseCommands(DataCmd);
+        }
+        else
+        {
+            foreach (string ValidUser in ValidUsers)
+            {
+                Log.Write("ValidUser: " + ValidUser);
+                if (ScenePrivate.FindAgent(Data.SourceId).AgentInfo.Name == ValidUser.Trim())
+                {
+                    string DataCmd = Data.Message;
+                    ParseCommands(DataCmd);
+                }
+            }
+        }
+
+    }
+
+    private void ParseCommands(string DataCmdIn)
+    {
+        Log.Write("DataCmdIn: " + DataCmdIn);
+        string CharToSendOut;
+        int cntr = 9;
+        do
+        {
+            CharToSendOut = DataCmdIn.Substring(cntr, 1);
+            SendChar sendChar = new SendChar();
+            sendChar.CharIndex = cntr-9;
+            sendChar.CharToSend = CharToSendOut; 
+            Log.Write("Sending Letter: " + CharToSendOut + " To Letter #: " + sendChar.CharIndex);
+            PostScriptEvent(ScriptId.AllScripts, "LetterSent", sendChar);  //used by synth
+            cntr++;
+        } while (cntr < DataCmdIn.Length);
+    }
+
+ 
+}
