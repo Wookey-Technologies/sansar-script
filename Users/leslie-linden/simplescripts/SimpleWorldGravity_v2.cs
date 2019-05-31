bc6f433fb7591b2555fbc0aac47335dee12e6082
Leslie Linden <leslie@lindenlab.com>
2019-05-31 14:18:14 -0700

    * Simple script example

---
+++ Users/leslie-linden/simplescripts/SimpleWorldGravity_v2.cs
@@ -0,0 +1,297 @@
+/* This content is licensed under the terms of the Creative Commons Attribution 4.0 International License.
+ * When using this content, you must:
+ * •    Acknowledge that the content is from the Sansar Knowledge Base.
+ * •    Include our copyright notice: "© 2017 Linden Research, Inc."
+ * •    Indicate that the content is licensed under the Creative Commons Attribution-Share Alike 4.0 International License.
+ * •    Include the URL for, or link to, the license summary at https://creativecommons.org/licenses/by-sa/4.0/deed.hi (and, if possible, to the complete license terms at https://creativecommons.org/licenses/by-sa/4.0/legalcode.
+ * For example:
+ * "This work uses content from the Sansar Knowledge Base. © 2017 Linden Research, Inc. Licensed under the Creative Commons Attribution 4.0 International License (license summary available at https://creativecommons.org/licenses/by/4.0/ and complete license terms available at https://creativecommons.org/licenses/by/4.0/legalcode)."
+ */
+
+using Sansar;
+using Sansar.Script;
+using Sansar.Simulation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class SimpleWorldGravity : SceneObjectScript
+{
+    #region EditorProperties
+    [Tooltip("Set default gravity")]
+    [DefaultValue("on")]
+    [DisplayName("-> Set Earth Gravity")]
+    public readonly string SetEarthGravityEvent;
+
+    [Tooltip("Set no gravity")]
+    [DefaultValue("off")]
+    [DisplayName("-> Set Zero Gravity")]
+    public readonly string SetZeroGravityEvent;
+
+    [Tooltip("Set gravity to setting A")]
+    [DefaultValue("set_gravity_{INDEX}")]
+    [DisplayName("-> Set Gravity By Index")]
+    public readonly string SetGravityIndexEvent;
+
+    [Tooltip("Gravity Settings in G's not m/s^2")]
+    [DisplayName("Gravity Settings (g)")]
+    [Range(0.0f, 5.0f)]
+    public readonly List<float> GravityValues;
+
+    [Tooltip("Enable responding to events for this script")]
+    [DefaultValue("world_gravity_enable")]
+    [DisplayName("-> Enable")]
+    public readonly string EnableEvent;
+
+    [Tooltip("Disable responding to events for this script")]
+    [DefaultValue("world_gravity_disable")]
+    [DisplayName("-> Disable")]
+    public readonly string DisableEvent;
+
+    [Tooltip(@"If StartEnabled is true then the script will respond to interactions when the scene is loaded
+If StartEnabled is false then the script will not respond to interactions until an (-> Enable) event is received.")]
+    [DefaultValue(true)]
+    [DisplayName("Start Enabled")]
+    public readonly bool StartEnabled = true;
+    #endregion
+
+    #region SimpleHelpers v2
+    [Tooltip(@"Update the region tag above by incrementing the version when updating anything in the region.
+If a Group is set, will only respond and send to other SimpleScripts with the same Group tag set.
+Does NOT accept CSV lists of groups.
+To send or receive events to/from a specific group from outside that group prepend the group name with a > to the event name, e.g. ""my_group>on""")]
+    [DefaultValue("")]
+    [DisplayName("Group")]
+    public string Group = "";
+
+    public interface ISimpleData
+    {
+        AgentInfo AgentInfo { get; }
+        ObjectId ObjectId { get; }
+        ObjectId SourceObjectId { get; }
+
+        // Extra data
+        Reflective ExtraData { get; }
+    }
+
+    public class SimpleData : Reflective, ISimpleData
+    {
+        public SimpleData(ScriptBase script) { ExtraData = script; }
+        public AgentInfo AgentInfo { get; set; }
+        public ObjectId ObjectId { get; set; }
+        public ObjectId SourceObjectId { get; set; }
+
+        public Reflective ExtraData { get; }
+    }
+
+    public interface IDebugger { bool DebugSimple { get; } }
+    private bool __debugInitialized = false;
+    protected bool __SimpleDebugging = false;
+    protected string __SimpleTag = "";
+
+    protected string GenerateEventName(string eventName)
+    {
+        eventName = eventName.Trim();
+        if (eventName.EndsWith("@"))
+        {
+            // Special case on@ to send the event globally (the null group) by sending w/o the @.
+            return eventName.Substring(0, eventName.Length - 1);
+        }
+        else if (Group == "" || eventName.Contains("@"))
+        {
+            // No group was set or already targeting a specific group as is.
+            return eventName;
+        }
+        else
+        {
+            // Append the group
+            return $"{eventName}@{Group}";
+        }
+    }
+
+    protected void SetupSimple()
+    {
+        __debugInitialized = true;
+        __SimpleTag = GetType().Name + " [S:" + Script.ID.ToString() + " O:" + ObjectPrivate.ObjectId.ToString() + "]";
+        Wait(TimeSpan.FromSeconds(1));
+        IDebugger debugger = ScenePrivate.FindReflective<IDebugger>("Simple.Debugger").FirstOrDefault();
+        if (debugger != null) __SimpleDebugging = debugger.DebugSimple;
+    }
+
+    System.Collections.Generic.Dictionary<string, Func<string, Action<ScriptEventData>, Action>> __subscribeActions = new System.Collections.Generic.Dictionary<string, Func<string, Action<ScriptEventData>, Action>>();
+    protected Action SubscribeToAll(string csv, Action<ScriptEventData> callback)
+    {
+        if (!__debugInitialized) SetupSimple();
+        if (string.IsNullOrWhiteSpace(csv)) return null;
+
+        Func<string, Action<ScriptEventData>, Action> subscribeAction;
+        if (__subscribeActions.TryGetValue(csv, out subscribeAction))
+        {
+            return subscribeAction(csv, callback);
+        }
+
+        // Simple case.
+        if (!csv.Contains(">>"))
+        {
+            __subscribeActions[csv] = SubscribeToAllInternal;
+            return SubscribeToAllInternal(csv, callback);
+        }
+
+        // Chaining
+        __subscribeActions[csv] = (_csv, _callback) =>
+        {
+            System.Collections.Generic.List<string> chainedCommands = new System.Collections.Generic.List<string>(csv.Split(new string[] { ">>" }, StringSplitOptions.RemoveEmptyEntries));
+
+            string initial = chainedCommands[0];
+            chainedCommands.RemoveAt(0);
+            chainedCommands.Add(initial);
+
+            Action unsub = null;
+            Action<ScriptEventData> wrappedCallback = null;
+            wrappedCallback = (data) =>
+            {
+                string first = chainedCommands[0];
+                chainedCommands.RemoveAt(0);
+                chainedCommands.Add(first);
+                if (unsub != null) unsub();
+                unsub = SubscribeToAllInternal(first, wrappedCallback);
+                Log.Write(LogLevel.Info, "CHAIN Subscribing to " + first);
+                _callback(data);
+            };
+
+            unsub = SubscribeToAllInternal(initial, wrappedCallback);
+            return unsub;
+        };
+
+        return __subscribeActions[csv](csv, callback);
+    }
+
+    protected Action SubscribeToAllInternal(string csv, Action<ScriptEventData> callback)
+    {
+        Action unsubscribes = null;
+        string[] events = csv.Trim().Split(new char[] { ',' }, StringSplitOptions.RemoveEmptyEntries);
+        if (__SimpleDebugging)
+        {
+            Log.Write(LogLevel.Info, __SimpleTag, "Subscribing to " + events.Length + " events: " + string.Join(", ", events));
+        }
+        Action<ScriptEventData> wrappedCallback = callback;
+
+        foreach (string eventName in events)
+        {
+            if (__SimpleDebugging)
+            {
+                var sub = SubscribeToScriptEvent(GenerateEventName(eventName), (ScriptEventData data) =>
+                {
+                    Log.Write(LogLevel.Info, __SimpleTag, "Received event " + GenerateEventName(eventName));
+                    wrappedCallback(data);
+                });
+                unsubscribes += sub.Unsubscribe;
+            }
+            else
+            {
+                var sub = SubscribeToScriptEvent(GenerateEventName(eventName), wrappedCallback);
+                unsubscribes += sub.Unsubscribe;
+            }
+        }
+        return unsubscribes;
+    }
+
+    System.Collections.Generic.Dictionary<string, Action<string, Reflective>> __sendActions = new System.Collections.Generic.Dictionary<string, Action<string, Reflective>>();
+    protected void SendToAll(string csv, Reflective data)
+    {
+        if (!__debugInitialized) SetupSimple();
+        if (string.IsNullOrWhiteSpace(csv)) return;
+
+        Action<string, Reflective> sendAction;
+        if (__sendActions.TryGetValue(csv, out sendAction))
+        {
+            sendAction(csv, data);
+            return;
+        }
+
+        // Simple case.
+        if (!csv.Contains(">>"))
+        {
+            __sendActions[csv] = SendToAllInternal;
+            SendToAllInternal(csv, data);
+            return;
+        }
+
+        // Chaining
+        System.Collections.Generic.List<string> chainedCommands = new System.Collections.Generic.List<string>(csv.Split(new string[] { ">>" }, StringSplitOptions.RemoveEmptyEntries));
+        __sendActions[csv] = (_csv, _data) =>
+        {
+            string first = chainedCommands[0];
+            chainedCommands.RemoveAt(0);
+            chainedCommands.Add(first);
+
+            Log.Write(LogLevel.Info, "CHAIN Sending to " + first);
+            SendToAllInternal(first, _data);
+        };
+        __sendActions[csv](csv, data);
+    }
+
+    protected void SendToAllInternal(string csv, Reflective data)
+    {
+        if (string.IsNullOrWhiteSpace(csv)) return;
+        string[] events = csv.Trim().Split(new char[] { ',' }, StringSplitOptions.RemoveEmptyEntries);
+
+        if (__SimpleDebugging) Log.Write(LogLevel.Info, __SimpleTag, "Sending " + events.Length + " events: " + string.Join(", ", events) + (Group != "" ? (" to group " + Group) : ""));
+        foreach (string eventName in events)
+        {
+            PostScriptEvent(GenerateEventName(eventName), data);
+        }
+    }
+    #endregion
+
+    Action unsubscribes = null;
+
+    public override void Init()
+    {
+        if (StartEnabled) SubscribeToMessages();
+
+        SubscribeToAll(EnableEvent, (ScriptEventData data) => { SubscribeToMessages(); });
+        SubscribeToAll(DisableEvent, (ScriptEventData data) => { UnsubscribeFromMessages(); });
+    }
+
+    void SubscribeToMessages()
+    {
+        const float OneG = 9.81f;
+
+        // Already subscribed
+        if (unsubscribes != null)
+        {
+            return;
+        }
+
+        unsubscribes += SubscribeToAll(SetEarthGravityEvent, (data) =>
+        {
+            ScenePrivate.SetGravity(OneG);
+        });
+
+        unsubscribes += SubscribeToAll(SetZeroGravityEvent, (data) =>
+        {
+            ScenePrivate.SetGravity(0.0f);
+        });
+
+        for (int i = 0; i < GravityValues.Count; i++)
+        {
+            int index = i;
+            string gravityIndexEventName = SetGravityIndexEvent.Replace("{INDEX}", index.ToString());
+
+            unsubscribes += SubscribeToAll(gravityIndexEventName, (data) =>
+            {
+                ScenePrivate.SetGravity(OneG * GravityValues[index]);
+            });
+        }
+    }
+
+    void UnsubscribeFromMessages()
+    {
+        if (unsubscribes != null)
+        {
+            unsubscribes();
+            unsubscribes = null;
+        }
+    }
+}
