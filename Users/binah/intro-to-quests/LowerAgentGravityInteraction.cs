9677be73441f08367b43b5de4fe56d309881cf59
Binah <binah@lindenlab.com>
2019-07-30 07:27:11 -0700

    update script based on feedback from kelly

--- Users/binah/intro-to-quests/LowerAgentGravityInteraction.cs
+++ Users/binah/intro-to-quests/LowerAgentGravityInteraction.cs
@@ -13,29 +13,23 @@ using Sansar.Script;
 using Sansar.Simulation;
 using System;
 
-// A small starter script.
+
 public class LowerAgentGravity : SceneObjectScript
 {
     #region EditorProperties
+    // An Interaction public property makes the script clickable.
     // This interaction will have a default prompt of "Click Me!"
     // Public fields show in the object properties after being added to a script.
-    // An Interaction public property makes the script clickable.
     [DefaultValue("Click Me!")]
-    public Interaction LowerAgentGravityInteraction;
+    public Interaction InteractionPrompt;
+    [DisplayName("Quest Command")]
+    public string questCommand;
+    [DisplayName("Gravity Factor")]
+    [DefaultValue(0.1f)]
+    public readonly float gravityFactor = 0.1f;
     #endregion
 
-    // Simple script compatible data structures
-    public interface ISimpleData
-    {
-        AgentInfo AgentInfo { get; }
-        ObjectId ObjectId { get; }
-        ObjectId SourceObjectId { get; }
-
-        // Extra data
-        Reflective ExtraData { get; }
-    }
-
-    public class SimpleData : Reflective, ISimpleData
+    public class SimpleData : Reflective
     {
         public SimpleData(ScriptBase script) { ExtraData = script; }
         public AgentInfo AgentInfo { get; set; }
@@ -50,33 +44,36 @@ public class LowerAgentGravity : SceneObjectScript
     // Init() is where the script is setup and is run when the script starts.
     public override void Init()
     {
+        // initialize simple data object to send events to other scripts
         _simpleData = new SimpleData(this);
-        _simpleData.ObjectId = ObjectPrivate.ObjectId;
-        _simpleData.SourceObjectId = ObjectPrivate.ObjectId;
 
         // Subscribe to interaction events to do something when the object is clicked.
-        LowerAgentGravityInteraction.Subscribe(OnClick);
+        InteractionPrompt.Subscribe(OnClick);
     }
 
     public void OnClick(InteractionData data)
     {
         // Find the agent that clicked.
         AgentPrivate agent = ScenePrivate.FindAgent(data.AgentId);
-        
-        // Lower gravity for the agent that clicked the button
-        agent.SetGravityFactor(0.05f);
 
-        SendQuestEvent("gravityLowered", agent);
-      
+        // Lower gravity for the agent that clicked
+        agent.SetGravityFactor(gravityFactor);
+
+        //Send the quest event
+        SendQuestEvent(questCommand, agent);
+
     }
 
     void SendQuestEvent(string eventName, AgentPrivate agent)
     {
         if (agent != null)
         {
+            //ObjectId and AgentInfo are used by other simple scripts to know who did it
             _simpleData.AgentInfo = agent.AgentInfo;
+            _simpleData.ObjectId = agent.AgentInfo.ObjectId;
         }
 
+        // Post script event
         PostScriptEvent(eventName, _simpleData);
     }
 
