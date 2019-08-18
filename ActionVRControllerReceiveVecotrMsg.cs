49b8d892d7b6f34393db9dce87beed63d0e248ee
Mike Suttom <mike.j.sutton@tx.rr.com>
2019-08-18 14:43:09 -0500

    Add files via upload

---
+++ ActionVRControllerReceiveVecotrMsg.cs
@@ -0,0 +1,322 @@
+﻿//* "This work uses content from the Sansar Knowledge Base. © 2017 Linden Research, Inc. Licensed under the Creative Commons Attribution 4.0 International License (license summary available at https://creativecommons.org/licenses/by/4.0/ and complete license terms available at https://creativecommons.org/licenses/by/4.0/legalcode)."
+
+using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Diagnostics;
+
+using Sansar;
+using Sansar.Script;
+using Sansar.Simulation;
+
+// This script is attached to a 3d model.  It has configuration paramters to identify control surfaces on the 3d model.  
+// A control surface is a portion of the model that when left mouse clicked on in desktop mode of touched with your hand and 
+// trigger in VR mode by the user it will send a simple message and a Reflex Bang that sends the message associated with the 
+// control surface.  A good example of this is that the 3d model is a drum set.  Each drum and cymbal you define a Control Surface
+// for.  Control Surfaces are Circles.  The Control Surfaces are configured using the followinng structure"
+// EventName, XcenterofControlSurface, YcenterofConntrolSurface, RadiusOfControlSurface, Zminimum, Zmaximum
+// SnareDrumHit, -12, 35, 25, 0, 200
+// The units above are in centimeters and they are all relative from the center of the model.  So, this means that the control surface
+// defined says that if the user hits on the model in an area that is within a circle located with an origin of X=-12cm, Y=35cm with a
+// radius of 25cm and anywhere from 0 to 200 cm on the Z axis a Simple Message Event named SnareDrumHit will be sent.
+//
+// This means that any Simple Script Effector could be listening for the Event SnareDrumHit and then execute things like turning on
+// a light, moving an object, generating a sound, etc.
+
+
+public class ActionVRControllerReceiveVectorMsg : SceneObjectScript
+
+{
+    #region ConstantsVariables
+    public float OffTimer = 0;
+
+    private RigidBodyComponent RigidBody = null;
+    private Vector CurPos = new Vector(0.0f, 0.0f, 0.0f);
+    private Vector CurCollisionCenter = new Vector(0.0f, 0.0f, 0.0f);
+    private Vector CurCollisionExtents = new Vector(0.0f, 0.0f, 0.0f);
+
+    #endregion
+
+    public override void Init()
+    {
+        Script.UnhandledException += UnhandledException; // Catch errors and keep running unless fatal
+
+        //SubscribeToScriptEvent("CollisionData", getCollisionData);
+
+        if (RigidBody == null)
+        {
+            if (!ObjectPrivate.TryGetFirstComponent(out RigidBody))
+            {
+                // Since object scripts are initialized when the scene loads, no one will actually see this message.
+                ScenePrivate.Chat.MessageAllUsers("There is no RigidBodyComponent attached to this object.");
+                return;
+            }
+        }
+
+        CurPos = RigidBody.GetPosition();
+        Log.Write("CurPos: " + CurPos);
+        CurCollisionCenter = RigidBody.GetColliderCenter();
+        Log.Write("CurCollisionCenter: " + CurCollisionCenter);
+        CurCollisionExtents = RigidBody.GetColliderExtents();
+        Log.Write("CurCollisionExtents: " + CurCollisionExtents);
+        SubscribeToScriptEvent("VRControllerInfo", getVRControllerInfo);
+
+    }
+
+    private void UnhandledException(object Sender, Exception Ex)
+    {
+        Log.Write(LogLevel.Error, GetType().Name, Ex.Message + "\n" + Ex.StackTrace + "\n" + Ex.Source);
+        return;
+    }//UnhandledException
+
+    #region Communication
+
+    #region SimpleHelpers v2
+    // Update the region tag above by incrementing the version when updating anything in the region.
+
+    // If a Group is set, will only respond and send to other SimpleScripts with the same Group tag set.
+    // Does NOT accept CSV lists of groups.
+    // To send or receive events to/from a specific group from outside that group prepend the group name with a > to the event name
+    // my_group>on
+    [DefaultValue("")]
+    [DisplayName("Group")]
+    public string Group = "";
+
+    public interface ISimpleData
+    {
+        AgentInfo AgentInfo { get; }
+        ObjectId ObjectId { get; }
+        ObjectId SourceObjectId { get; }
+
+        // Extra data
+        Reflective ExtraData { get; }
+    }
+
+    public class SimpleData : Reflective, ISimpleData
+    {
+        public SimpleData(ScriptBase script) { ExtraData = script; }
+        public AgentInfo AgentInfo { get; set; }
+        public ObjectId ObjectId { get; set; }
+        public ObjectId SourceObjectId { get; set; }
+
+        public Reflective ExtraData { get; }
+    }
+
+    public interface IDebugger { bool DebugSimple { get; } }
+    private bool __debugInitialized = false;
+    private bool __SimpleDebugging = false;
+    private string __SimpleTag = "";
+
+    private string GenerateEventName(string eventName)
+    {
+        eventName = eventName.Trim();
+        if (eventName.EndsWith("@"))
+        {
+            // Special case on@ to send the event globally (the null group) by sending w/o the @.
+            return eventName.Substring(0, eventName.Length - 1);
+        }
+        else if (Group == "" || eventName.Contains("@"))
+        {
+            // No group was set or already targeting a specific group as is.
+            return eventName;
+        }
+        else
+        {
+            // Append the group
+            return $"{eventName}@{Group}";
+        }
+    }
+
+    private void SetupSimple()
+    {
+        __debugInitialized = true;
+        __SimpleTag = GetType().Name + " [S:" + Script.ID.ToString() + " O:" + ObjectPrivate.ObjectId.ToString() + "]";
+        Wait(TimeSpan.FromSeconds(1));
+        IDebugger debugger = ScenePrivate.FindReflective<IDebugger>("SimpleDebugger").FirstOrDefault();
+        if (debugger != null) __SimpleDebugging = debugger.DebugSimple;
+    }
+
+    System.Collections.Generic.Dictionary<string, Func<string, Action<ScriptEventData>, Action>> __subscribeActions = new System.Collections.Generic.Dictionary<string, Func<string, Action<ScriptEventData>, Action>>();
+    private Action SubscribeToAll(string csv, Action<ScriptEventData> callback)
+    {
+        if (!__debugInitialized) SetupSimple();
+        if (string.IsNullOrWhiteSpace(csv)) return null;
+
+        Func<string, Action<ScriptEventData>, Action> subscribeAction;
+        if (__subscribeActions.TryGetValue(csv, out subscribeAction))
+        {
+            return subscribeAction(csv, callback);
+        }
+
+        // Simple case.
+        if (!csv.Contains(">>"))
+        {
+            __subscribeActions[csv] = SubscribeToAllInternal;
+            return SubscribeToAllInternal(csv, callback);
+        }
+
+        // Chaining
+        __subscribeActions[csv] = (_csv, _callback) =>
+        {
+            System.Collections.Generic.List<string> chainedCommands = new System.Collections.Generic.List<string>(csv.Split(new string[] { ">>" }, StringSplitOptions.RemoveEmptyEntries));
+
+            string initial = chainedCommands[0];
+            chainedCommands.RemoveAt(0);
+            chainedCommands.Add(initial);
+
+            Action unsub = null;
+            Action<ScriptEventData> wrappedCallback = null;
+            wrappedCallback = (data) =>
+            {
+                string first = chainedCommands[0];
+                chainedCommands.RemoveAt(0);
+                chainedCommands.Add(first);
+                if (unsub != null) unsub();
+                unsub = SubscribeToAllInternal(first, wrappedCallback);
+                Log.Write(LogLevel.Info, "CHAIN Subscribing to " + first);
+                _callback(data);
+            };
+
+            unsub = SubscribeToAllInternal(initial, wrappedCallback);
+            return unsub;
+        };
+
+        return __subscribeActions[csv](csv, callback);
+    }
+
+    private Action SubscribeToAllInternal(string csv, Action<ScriptEventData> callback)
+    {
+        Action unsubscribes = null;
+        string[] events = csv.Trim().Split(new char[] { ',' }, StringSplitOptions.RemoveEmptyEntries);
+        if (__SimpleDebugging)
+        {
+            Log.Write(LogLevel.Info, __SimpleTag, "Subscribing to " + events.Length + " events: " + string.Join(", ", events));
+        }
+        Action<ScriptEventData> wrappedCallback = callback;
+
+        foreach (string eventName in events)
+        {
+            if (__SimpleDebugging)
+            {
+                var sub = SubscribeToScriptEvent(GenerateEventName(eventName), (ScriptEventData data) =>
+                {
+                    Log.Write(LogLevel.Info, __SimpleTag, "Received event " + GenerateEventName(eventName));
+                    wrappedCallback(data);
+                });
+                unsubscribes += sub.Unsubscribe;
+            }
+            else
+            {
+                var sub = SubscribeToScriptEvent(GenerateEventName(eventName), wrappedCallback);
+                unsubscribes += sub.Unsubscribe;
+            }
+        }
+        return unsubscribes;
+    }
+
+    System.Collections.Generic.Dictionary<string, Action<string, Reflective>> __sendActions = new System.Collections.Generic.Dictionary<string, Action<string, Reflective>>();
+    private void SendToAll(string csv, Reflective data)
+    {
+        if (!__debugInitialized) SetupSimple();
+        if (string.IsNullOrWhiteSpace(csv)) return;
+
+        Action<string, Reflective> sendAction;
+        if (__sendActions.TryGetValue(csv, out sendAction))
+        {
+            sendAction(csv, data);
+            return;
+        }
+
+        // Simple case.
+        if (!csv.Contains(">>"))
+        {
+            __sendActions[csv] = SendToAllInternal;
+            SendToAllInternal(csv, data);
+            return;
+        }
+
+        // Chaining
+        System.Collections.Generic.List<string> chainedCommands = new System.Collections.Generic.List<string>(csv.Split(new string[] { ">>" }, StringSplitOptions.RemoveEmptyEntries));
+        __sendActions[csv] = (_csv, _data) =>
+        {
+            string first = chainedCommands[0];
+            chainedCommands.RemoveAt(0);
+            chainedCommands.Add(first);
+
+            Log.Write(LogLevel.Info, "CHAIN Sending to " + first);
+            SendToAllInternal(first, _data);
+        };
+        __sendActions[csv](csv, data);
+    }
+
+    private void SendToAllInternal(string csv, Reflective data)
+    {
+        if (string.IsNullOrWhiteSpace(csv)) return;
+        string[] events = csv.Trim().Split(new char[] { ',' }, StringSplitOptions.RemoveEmptyEntries);
+
+        if (__SimpleDebugging) Log.Write(LogLevel.Info, __SimpleTag, "Sending " + events.Length + " events: " + string.Join(", ", events) + (Group != "" ? (" to group " + Group) : ""));
+        foreach (string eventName in events)
+        {
+            PostScriptEvent(GenerateEventName(eventName), data);
+        }
+    }
+    #endregion
+
+    public interface VRControllerInfo
+    {
+        AgentPrivate VRAgent { get; }
+        string VRController { get; }
+        Vector VRControllerLocalPosition { get; }
+        Vector VRControllerWorldPosition { get; }
+    }
+
+    private void getVRControllerInfo(ScriptEventData gotVRControllerInfo)
+    {
+        //Log.Write("getVRControllerInfo");
+        if (gotVRControllerInfo.Data == null)
+        {
+            return;
+        }
+
+        VRControllerInfo sendVRControllerInfo = gotVRControllerInfo.Data.AsInterface<VRControllerInfo>();
+
+        if (sendVRControllerInfo == null)
+        {
+            Log.Write(LogLevel.Error, Script.ID.ToString(), "Unable to create interface, check logs for missing member(s)");
+            return;
+        }
+
+        AgentPrivate VRAgent = sendVRControllerInfo.VRAgent;
+        string VRController = sendVRControllerInfo.VRController;
+        Vector VRControllerLocalPosition = sendVRControllerInfo.VRControllerLocalPosition;
+        Vector VRControllerWorldPosition = sendVRControllerInfo.VRControllerWorldPosition;
+
+        Log.Write("Message Received VRAgent: " + VRAgent.AgentInfo.Name + " VRController: " + VRController + " LocalPos: " + VRControllerLocalPosition + " WorldPos: " + VRControllerWorldPosition);
+
+        //Interact with hot spots calls go here
+
+    }
+
+    private void sendSimpleMessage(string msg, InteractionData data)
+    {
+        SimpleData sd = new SimpleData(this);
+        sd.AgentInfo = ScenePrivate.FindAgent(data.AgentId)?.AgentInfo;
+        sd.ObjectId = sd.AgentInfo.ObjectId;
+        sd.SourceObjectId = ObjectPrivate.ObjectId;
+        SendToAll(msg, sd);
+        if (OffTimer > 0)
+        {
+            Wait(TimeSpan.FromMilliseconds((int)OffTimer * 1000));
+            SendToAll(msg + "Off", sd);
+        }
+    }
+
+    #endregion
+
+    #region Interaction
+
+    // This is where the unique actions that would be triggered by the VR Controller interaction would take place (turn on lights, play sound, move object, etc)
+    // This also could act as as a logic module and send various simple messages based on the position from the VR controller (example, send message to a instrument)
+
+    #endregion
+}
