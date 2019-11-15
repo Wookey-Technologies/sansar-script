ac804dd721be6ca4d28622dd11a5222f6b1430b2
EvoAv <darwinrecreant@gmail.com>
2019-11-15 13:13:58 +0000

    Safe data store

---
+++ Users/evoav/SafeDataStore/Example.cs
@@ -0,0 +1,31 @@
+using Sansar.Simulation;
+using System;
+using Sansar.Script;
+
+namespace Persistence {
+  class ExampleDatabase : SceneObjectScript {
+    public string DatabaseName;
+    Func<string, DataStore> createTable = null;
+
+    override public void Init() {
+      float waitAmount = 0.05f;
+      while (waitAmount < 4) {
+        foreach (IDatabase db in ScenePrivate.FindReflective<IDatabase>("Persistence.Database")) {
+          createTable = db.GetCreateTable(DatabaseName);
+          if (createTable != null) break;
+        }
+        Wait(waitAmount);
+        waitAmount += waitAmount;
+      }
+
+      if (createTable == null) {
+        Log.Write(LogLevel.Warning, ObjectPrivate.Name, "Database not found: " + DatabaseName);
+        return;
+      }
+
+      DataStore users = createTable("users");
+      DataStore highScore = createTable("highscore");
+      // ...
+    }
+  }
+}
